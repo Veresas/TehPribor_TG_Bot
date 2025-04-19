0262089082d318e32ba367264831dd4fb946121b
Alexander_Ozhered <Ozhereda@mail.ru>
2025-04-19 17:06:25 +0300

    fast fix

--- bot/app/hendlers/public.py
+++ bot/app/hendlers/public.py
@@ -45,6 +45,18 @@ COMMANDS_BY_ROLE = {
        BotCommand(command="reload_comand", description="Обновление списка команд"),
        BotCommand(command="change_role", description="Смена роли")
     ],
+    "Мастер_админ": [
+       BotCommand(command="start", description="Запустить бота"),
+       BotCommand(command="help", description="Помощь"),
+       BotCommand(command="my_orders", description="Ваш список заказов"),
+       BotCommand(command="orders", description="Список всех заказов"),
+       BotCommand(command="new_order", description="Создание нового заказа"),
+       BotCommand(command="cancel", description="Отмена команды"),
+       BotCommand(command="export", description="Импорт данных о заказах в Exel"),
+       BotCommand(command="admin_panel", description="Панель администратора"),
+       BotCommand(command="reload_comand", description="Обновление списка команд"),
+       BotCommand(command="change_role", description="Смена роли")
+    ]
 }
 
 async def set_user_commands(bot: Bot, tg_id: int):
